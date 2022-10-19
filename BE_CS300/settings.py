badb396ede27cd24e2e5ad06f81c7a9a8894307d
Phuc-eoh <114310451+phuc-lychee@users.noreply.github.com>
2022-10-19 23:23:02 +0700

    database

--- BE_CS300/settings.py
+++ BE_CS300/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'BE_CS300.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cs300',
+        'USER': 'cs300',
+        'PASSWORD': 'cs300',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
