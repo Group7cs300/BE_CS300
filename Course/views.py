111992645f58bf693832a687f46b6a5a9a31750e
tupeo31072002@gmail.com <pttu20@apcs.fitus.edu.vn>
2022-10-27 08:57:13 +0700

    create API Course

--- Course/views.py
+++ Course/views.py
@@ -9,7 +9,7 @@ from django.http import JsonResponse
 
 # Create your views here.
 
-@api_view(['GET','POST'])
+@api_view(['GET', 'POST'])
 def get_course(request):
     if request.method == 'GET':
         courses = Course.objects.all()
@@ -22,3 +22,23 @@ def get_course(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response({"invalid": "not good data"}, status=404)
+
+
+@api_view(['PUT','DELETE'])
+def update(request,tutor_id, course_id):
+    try:
+        course = Course.objects.filter(tutor_id=tutor_id,id=course_id).first()
+    except Course.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = CourseSerializer(course, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        course.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
