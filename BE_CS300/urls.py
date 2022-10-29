49c5c370fd5815eb91c348f699cd035ea4babc81
phuc-lychee <114310451+phuc-lychee@users.noreply.github.com>
2022-10-29 10:30:07 +0700

    [I2G-11] Course Model (#5)

--- BE_CS300/urls.py
+++ BE_CS300/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView
 from drf_spectacular.views import SpectacularSwaggerView
+from rest_framework.routers import SimpleRouter
 
 from app.views import HealthCheckView
+from course.views.course import CourseViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,3 +14,8 @@ urlpatterns = [
     # Optional UI:
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui')
 ]
+
+router = SimpleRouter(trailing_slash=False)
+router.register(r'^course', CourseViewSet)
+
+urlpatterns += router.urls
---
+++ course/models/course.py
@@ -0,0 +1,14 @@
+from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
+from django.db import models
+
+from app.models import Account
+from app.models import BaseModel
+
+
+class Course(BaseModel):
+    name = models.CharField(max_length=250)
+    is_finished = models.BooleanField(default=False)
+    tutor = models.ForeignKey(Account, related_name='courses', on_delete=models.CASCADE)
+    price = models.IntegerField(default=1, validators=[MaxValueValidator(500), MinValueValidator(1)])
+    cover_image = models.ImageField(upload_to='courses/covers', null=True)
