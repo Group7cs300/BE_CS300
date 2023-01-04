ac6612ec163d3fa09f341433036b7c437e038b28
Phuc-eoh <ttphuc18@apcs.fitus.edu.vn>
2023-01-04 21:08:31 +0700

    Update urls.py

--- BE_CS300/urls.py
+++ BE_CS300/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
@@ -7,19 +5,16 @@ from drf_spectacular.views import SpectacularAPIView
 from drf_spectacular.views import SpectacularSwaggerView
 from rest_framework.routers import SimpleRouter
 
-from app.utils.environment import is_local
 from app.views.health_check import HealthCheckView
 from course.views.category import CategoryViewSet
 from course.views.course import CourseViewSet
 from course.views.owned_course import OwnedCourseViewSet
 from course.views.section import SectionViewSet
-from course.views.category import CategoryViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('healthcheck/', HealthCheckView.as_view()),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('app/', include('app.urls')),
 ]
@@ -29,6 +24,5 @@ router.register(r'^course', CourseViewSet)
 router.register(r'^owned_courses', OwnedCourseViewSet)
 router.register(r'^categories', CategoryViewSet)
 router.register(r'^sections', SectionViewSet)
-router.register(r'^category', CategoryViewSet)
 
 urlpatterns += router.urls
